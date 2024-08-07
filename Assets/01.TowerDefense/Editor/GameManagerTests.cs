84e1de5015e432bb058ab74781fd9f73637417f5
y2no12 <y2no12@gmail.com>
2024-08-07 11:03:41 +0900

    프로그램 전체 세팅 및 커밋
    프로그램 전체 세팅 및 커밋

---
+++ Assets/01.TowerDefense/Editor/GameManagerTests.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+
+[TestFixture]
+public class GameManagerTests  {
+	private GameManager gameManager;
+	private Enemy enemy;
+
+	[Test]
+	public void CheckRewardMathTest () {
+		gameManager = GameObject.FindObjectOfType<GameManager>();
+		Debug.Log("this bitch: " + gameManager.TotalMoney);
+		gameManager.TotalMoney = 5;
+		var rewardAmt = 2;
+		gameManager.addMoney(rewardAmt);
+		Assert.That(gameManager.TotalMoney, Is.EqualTo(7).Within(0.000001));
+		gameManager.subtractMoney(rewardAmt);
+		Assert.That(gameManager.TotalMoney, Is.EqualTo(5).Within(0.000001));
+	}
+
+	[Test]
+	public void CheckCurrentGameStateTest() {
+		gameManager = GameObject.FindObjectOfType<GameManager>();
+		gameManager.WaveNumber = 0;	
+		gameManager.TotalEscaped = 0;	
+		gameManager.setCurrentGameState();
+		Assert.That(gameManager.CurrentState, Is.EqualTo(gameStatus.play));
+
+		gameManager.WaveNumber = 1;
+		gameManager.TotalEscaped = 0;
+		gameManager.setCurrentGameState();
+		Assert.That(gameManager.CurrentState, Is.EqualTo(gameStatus.next));
+		
+		gameManager.WaveNumber = 4;
+		gameManager.TotalEscaped = 10;
+		gameManager.setCurrentGameState();
+		Assert.That(gameManager.CurrentState, Is.EqualTo(gameStatus.gameover));
+	
+		gameManager.WaveNumber = 10;
+		gameManager.TotalEscaped = 5;
+		gameManager.setCurrentGameState();
+		Assert.That(gameManager.CurrentState, Is.EqualTo(gameStatus.win));	
+	}
+
+	[Test]
+	public void RegisterEnemiesTest() {
+		gameManager = GameObject.FindObjectOfType<GameManager>();
+		enemy = Enemy.FindObjectOfType<Enemy>();
+		
+		gameManager.EnemyList.Clear();
+		
+		gameManager.RegisterEnemy(enemy);
+		Assert.That(gameManager.EnemyList.Count, Is.EqualTo(1).Within(0.000001));
+
+		gameManager.RegisterEnemy(enemy);
+		Assert.That(gameManager.EnemyList.Count, Is.EqualTo(2).Within(0.000001));	
+	}
+
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Tower-Defense-Game
+2D Tower Defense game made with Unity (C#)
+
+
+![Screenshot](TowerDefense.gif)
